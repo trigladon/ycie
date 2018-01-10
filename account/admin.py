a07993f31f9f5b9a9b147e1222681299d338bd6d
BVadim <trigladon@gmail.com>
2018-01-10 16:21:17 +0300

    Added account user to admin area

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import \
+    UserAdmin as BaseUserAdmin,\
+    UserCreationForm as BaseUserCreationForm, \
+    UserChangeForm as BaseUserChangeForm
 
-# Register your models here.
+from .models import User
+
+
+class UserCreationForm(BaseUserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+class UserChangeForm(BaseUserChangeForm):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+
+
+admin.site.register(User, UserAdmin)
