816bea5c025719f838167836aa09240d8dc97b6d
BVadim <trigladon@gmail.com>
2018-01-25 17:51:33 +0300

    small fixes

--- application/models.py
+++ application/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from account.models import User
 from house.models import House
-from .helper import upload_file, get_storage_path
+from .helper import upload_file
 from .storage import get_file_storage
 from .constants import *
 
@@ -18,10 +19,12 @@ class Asset(models.Model):
     )
 
     title = models.CharField(verbose_name=_("Title"), max_length=511, blank=True, null=True)
-    file = models.FileField(verbose_name=_("File"), upload_to=upload_file, null=False, blank=False, storage=get_file_storage())
+    file = models.FileField(verbose_name=_("File"), upload_to=upload_file, null=False, blank=False,
+                            storage=get_file_storage())
     file_real_name = models.TextField(_("Real file name"), null=False, blank=False)
     is_published = models.BooleanField(_("Is published"), null=False, blank=False)
-    type = models.CharField(_("Model type"), choices=ASSET_TYPES, default=ASSET_TYPE_NEWS, max_length=15, null=True, blank=True)
+    type = models.CharField(_("Model type"), choices=ASSET_TYPES, default=ASSET_TYPE_NEWS, max_length=15, null=True,
+                            blank=True)
     updated_date = models.DateTimeField(_("updated date"), default=timezone.now)
     created_date = models.DateTimeField(_("created date"), default=timezone.now, editable=False)
 
@@ -57,7 +60,8 @@ class City(models.Model):
     """
     City model
     """
-    country_id = models.ForeignKey(Country, blank=False, null=False, on_delete=models.CASCADE, verbose_name=_('County id'))
+    country_id = models.ForeignKey(Country, blank=False, null=False, on_delete=models.CASCADE,
+                                   verbose_name=_('County id'))
     name = models.CharField(_('City name'), null=False, blank=False, max_length=150)
     english_name = models.CharField(_('English Name'), max_length=150, null=False, blank=False)
     short_name = models.CharField(_('Short name'), max_length=50, null=False, blank=False)
@@ -118,6 +122,9 @@ class Contact(models.Model):
 
 
 class CategoryNews(models.Model):
+    """
+    Model for news category
+    """
     house = models.ForeignKey(House, verbose_name=_('House'), null=True, on_delete=models.CASCADE)
     title = models.CharField(verbose_name=_('Title'), null=False, max_length=1023)
     shot_description = models.TextField(verbose_name=_('Shot description'), null=True)
@@ -139,8 +146,66 @@ class CategoryNews(models.Model):
 
 
 class News(models.Model):
-    pass
+    """
+    Model for news
+    """
+    title = models.CharField(verbose_name=_('Title'), null=False, max_length=1023)
+    shot_description = models.TextField(verbose_name=_('Shot description'), null=True)
+    description = models.TextField(verbose_name=_('Description'), null=True)
+    is_published = models.BooleanField(verbose_name=_('Is published?'), default=True)
+    update_date = models.DateTimeField(_('Update date'), default=timezone.now, editable=False)
+    create_date = models.DateTimeField(_('Create date'), default=timezone.now, editable=False)
+
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.pk:
+            self.update_date = timezone.now()
+        return super(News, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                      update_fields=update_fields)
 
 
 class Voting(models.Model):
-    pass
+    """
+    Model for voting
+    """
+    title = models.CharField(verbose_name=_('Title'), null=False, max_length=1023)
+    description = models.TextField(verbose_name=_('Description'), null=True)
+    type = models.CharField(_('Type'), null=False, choices=())
+    update_date = models.DateTimeField(_('Update date'), default=timezone.now, editable=False)
+    create_date = models.DateTimeField(_('Create date'), default=timezone.now, editable=False)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.pk:
+            self.update_date = timezone.now()
+        return super(Voting, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                        update_fields=update_fields)
+
+    def to_dict(self):
+        return {
+
+        }
+
+
+class VotingOption(models.Model):
+    """
+    Options for voting
+    """
+    user = models.ManyToManyField(verbose_name=_('Users'), null=True, to=User, unique=True)
+    voting = models.ForeignKey(Voting, verbose_name=_('Voting'), null=False, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('Title'), null=False, max_length=1023)
+    description = models.CharField(verbose_name=_('Description'), null=False, max_length=1535)
+    amount = models.IntegerField(verbose_name=_('Amount'), default=0, null=False)
+
+    class Meta:
+        verbose_name = _("VotingOptions")
+        verbose_name_plural = _("VotingOptions")
+
+    def to_dict(self):
+        return {
+
+        }
